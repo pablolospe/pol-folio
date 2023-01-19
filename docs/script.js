2a7fecdf51bb3a2b56d2ea9b3e31b84b1adc2fea
pablolospe <yosoypxl@gmail.com>
2023-01-19 11:22:09 -0300

    responsive script

---
+++ docs/script.js
@@ -0,0 +1,8 @@
+function responsiveManu(){
+    var x = document.getElementById('nav');
+    if(x.className===''){
+        x.className = 'responsive';
+    }else{
+        x.className = ''
+    }
+}
