ad6a414a0ee2b243219e836fb4b1e1ae930b4abe
pablolospe <yosoypxl@gmail.com>
2023-03-16 23:35:35 -0300

    esconder menu responsive

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,14 @@
 function responsiveMenu(){
-    var x = document.getElementById('nav');
+    let x = document.getElementById('nav');
     if(x.className===''){
         x.className = 'responsive';
     }else{
         x.className = ''
     }
 }
+
+function cerrar(){
+    let y = document.getElementById('nav')
+    y.className='nav'
+    y.className=''
+  }
